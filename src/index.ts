4c11933088a5b09f1188e919e44a149f5c73b1b6
Blake Neely <122914646+Arvotius@users.noreply.github.com>
2024-09-16 18:11:26 -0400

    Update index.ts for ramp up

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { getRepoContributors, getRepoPullRequests } from "./API/githubAPI.js";
 import { calculateBusFactor } from "./metrics/busFactor.js";
 import { calculateCorrectness } from './metrics/correctness.js';
 import { calculateResponsiveMaintainer } from "./metrics/responsiveMaintainer.js";
+import { calculateRampUp } from "./metrics/rampUp.ts"; 
 
 const world = 'world!';
 
