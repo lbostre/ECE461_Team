edbcff235fe03cb44025e2e8fa9c1ea2cf2e3044
Leyton Bostre <leytonbostre@gmail.com>
2024-09-18 20:49:19 -0400

    some testing stuff

--- src/__tests__/busFactor.test.ts
+++ src/__tests__/busFactor.test.ts
@@ -1,6 +1,28 @@
 import {expect, test} from "vitest"
 import { calculateBusFactor } from '../metrics/busFactor';
 
-test("bus factor for yargs to equal .1333", async () => {
-    expect(await calculateBusFactor("yargs", "yargs")).toBe(.1333)
+test("bus factor for Wat4hjs to be low (less than or equal .33)", async () => {
+    expect(await calculateBusFactor("hasansultan92", "watch.js")).toBeLessThanOrEqual(.33)
+}, 60000)
+
+test("bus factor for 3js to be high (greater than or equal to .67)", async () => {
+    expect(await calculateBusFactor("mrdoob", "three.js")).toBeGreaterThanOrEqual(.67)
+}, 60000)
+
+test("bus factor for SocketIO to be medium (in between .33 and .67)", async () => {
+    const result = await calculateBusFactor("socketio", "socket.io")
+    expect(result).toBeGreaterThanOrEqual(.33)
+    expect(result).toBeLessThanOrEqual(.67)
+}, 60000)
+
+test("bus factor for libvlc to be low (less than or equal .33)", async () => {
+    expect(await calculateBusFactor("prathameshnetake", "libvlc")).toBeLessThanOrEqual(.33)
+}, 60000)
+
+test("bus factor for ReactJs to be high (greater than or equal to .67)", async () => {
+    expect(await calculateBusFactor("facebook", "react")).toBeGreaterThanOrEqual(.67)
+}, 60000)
+
+test("bus factor for unlicensed to be low (less than or equal .33)", async () => {
+    expect(await calculateBusFactor("ryanve", "unlicensed")).toBeLessThanOrEqual(.33)
 }, 60000)
--- src/__tests__/correctness.test.ts
+++ src/__tests__/correctness.test.ts
@@ -1,6 +1,7 @@
 import {expect, test} from "vitest"
 import { calculateCorrectness } from '../metrics/correctness';
 
-test("bus factor for yargs to equal .1333", async () => {
-    expect(await calculateCorrectness("microsoft", "Typescript")).toBe(.1333)
+test("correctness for Wat4hjs to be low (less than or equal .33)", async () => {
+    expect(await calculateCorrectness("https://www.npmjs.com/package/browserify", "hasansultan92", "watch.js")).toBeLessThanOrEqual(.7)
 }, 60000)
+
